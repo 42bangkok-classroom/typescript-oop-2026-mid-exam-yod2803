79aaea6a5cbecfbc3211683624b9ff44c4f3887a
Your Name <68011174@kmitl.ac.th>
2026-01-21 15:48:26 +0700

    lrk

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,13 @@
-// Write your code below
+const n: number = parseFloat(process.argv[2]);
+if (isNaN(n) || n < 1 || !Number.isInteger(n)) {
+    console.log("Invalid Input")
+} else {
+
+    for (let i = n; i > 0; i--) {
+        let line = ""
+        for (let j = i; j > 0; j--) {
+            line += j;
+        }
+        console.log(line)
+    }
+}
