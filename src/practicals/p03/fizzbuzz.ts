3efe06c4ac85611f780c6c45663b3fa6716b0abb
Your Name <68011174@kmitl.ac.th>
2026-01-21 16:12:07 +0700

    foesko

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -2,7 +2,7 @@ i = Number(process.argv[2]);
 
 const n = Number(i);
 
-if (Number.isNaN(i) || i < 0 ) 
+if (Number.isNaN(i) || i <= 0 ) 
     console.log("Invalid input");
 
 for (let i = 1; i <= n; i++) {
