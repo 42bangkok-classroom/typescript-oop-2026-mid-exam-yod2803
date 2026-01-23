fad8902337d88d572f3655c5cee4312fed4ea48b
viruskizz <kizzaraiva@gmail.com>
2026-01-23 18:11:38 +0700

    n m

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,8 +1,8 @@
 const x = Number(process.argv[2]);
-const n = Number(x);
+const m = Number(x);
 
 
-for (let j = 1; j <= n; j++) {
+for (let j = 1; j <= m; j++) {
 
     if (j % 3 == 0 && j % 5 ==0 ) {
         console.log("FizzBuzz");
